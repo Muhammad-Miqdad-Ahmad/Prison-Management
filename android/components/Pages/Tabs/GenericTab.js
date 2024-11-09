d2bebaff70b4088c7d7adf767051809632b5558b
Muhammad-Miqdad-Ahmad <muhammadmiqdadahmad@gmail.com>
2024-11-09 18:52:42 +0500

    Made the drop down menu and also completed the add prisoner screen..... I think

---
+++ android/components/Pages/Tabs/GenericTab.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Keyboard, ScrollView, TouchableWithoutFeedback } from "react-native";
+import { List } from "react-native-paper";
+
+const dismissKeyboard = () => {
+  Keyboard.dismiss();
+};
+
+export const GenericTab = ({ route }) => {
+  const [expanded, setExpanded] = useState(null);
+  const [data, setData] = useState({});
+
+  const handlePress = (index) => {
+    setExpanded(expanded === index ? null : index);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <ScrollView>
+        {route?.params?.components?.map((component, index) => {
+          const text = route?.params?.text?.[index];
+
+          return typeof text === "string" && text ? (
+            <List.Accordion
+              title={text}
+              expanded={expanded === index}
+              onPress={() => handlePress(index)}
+              key={index}
+            >
+              {React.cloneElement(component, { key: index, data, setData })}
+            </List.Accordion>
+          ) : (
+            React.cloneElement(component, {
+              key: index,
+              data,
+              setData,
+            })
+          );
+        })}
+      </ScrollView>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default GenericTab;
