b7fd6458cc251e41f934bb4b6fc832537225b69f
Muhammad-Miqdad-Ahmad <muhammadmiqdadahmad@gmail.com>
2024-11-11 09:44:09 +0500

    made fixes to some file names in the backend

---
+++ server/Controllers/GuardController.js
@@ -0,0 +1,8 @@
+const client = require("../Connections/Connections");
+const HttpStatusCodes = require("../Controllers/HttpRequests");
+
+const SomeFunction = async (req, res)=>{
+
+}
+
+module.exports = { SomeFunction, funcfautdf }
