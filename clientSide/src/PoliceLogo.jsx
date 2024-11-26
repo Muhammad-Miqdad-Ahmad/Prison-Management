c90507e76d009b5015dc08f3be1657f43bf2dcda
arhamtahir007 <bsce22007@itu.edu.pk>
2024-11-26 12:52:54 +0500

    speed changed

--- clientSide/src/PoliceLogo.jsx
+++ clientSide/src/PoliceLogo.jsx
@@ -1,7 +1,6 @@
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-import "./App.css";
 
 // Component for the rotating GLTF object
 const RotatingModel = ({ url, position }) => {
@@ -10,23 +9,31 @@ const RotatingModel = ({ url, position }) => {
 
   useFrame(() => {
     if (ref.current) {
-      ref.current.rotation.z += 0.01; // Rotate the model on the Y-axis
+      ref.current.rotation.z += 0.015; // Rotate the model on the Y-axis
     }
   });
 
   return <primitive object={model.scene} ref={ref} position={position} />;
 };
 
+function Mouse() {
+  const { mouse } = useThree();
+
+  return useFrame(() => {
+    console.log(mouse.x, mouse.y, mouse.z);
+  });
+}
+
 // Main App Component
-function App() {
+function PoliceLogo() {
   return (
     <Canvas camera={{ position: [0, 9, 0] }}>
-      <directionalLight position={[0, 9, 10]} intensity={4} />
+      <directionalLight position={[4, 12, 0]} intensity={1} />
       <ambientLight intensity={0.5} />
+      <Mouse/>
       <RotatingModel url="policeLogo.gltf" position={[0, 0, 0]} />
-      {/* <OrbitControls /> */}
     </Canvas>
   );
 }
 
-export default App;
+export default PoliceLogo;
